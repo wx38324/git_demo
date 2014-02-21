ff5a5362db5061be475c56c5fed058a6ff34b5ea
wx38324 <361237110@qq.com>
2014-02-21 13:34:49 +0800

    第三次git提交

--- src/org/test.java
+++ src/org/test.java
@@ -2,6 +2,7 @@ package org;
 
 public class test {
 	public static void main(String[] args) {
+		System.out.println("-------------");
 		System.out.println("Hello World!");
 		System.out.println("-------------");
 	}
