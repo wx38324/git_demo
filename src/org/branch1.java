e3bee925f93832a5cefc1d625765b1573834b888
wx38324 <361237110@qq.com>
2014-02-28 10:05:45 +0800

    Update branch1.java

--- src/org/branch1.java
+++ src/org/branch1.java
@@ -4,5 +4,6 @@ public class branch1 {
 	public static void main(String[] args) {
 		System.out.println("------branch1------");
 		System.out.println("------branch2------");
+		System.out.println("------branch3------");
 	}
 }
