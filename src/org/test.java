9d36819d46b323f01f2666396afb6f3813cc2c97
wx38324 <361237110@qq.com>
2014-02-28 09:38:02 +0800

    Signed-off-by: wx38324 <361237110@qq.com>

--- src/org/test.java
+++
@@ -1,9 +0,0 @@
-package org;
-
-public class test {
-	public static void main(String[] args) {
-		System.out.println("-------------");
-		System.out.println("Hello World!");
-		System.out.println("-------------");
-	}
-}
