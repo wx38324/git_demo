d104f765e0301661be2ebc110dea2ef8ecadfda0
wx38324 <361237110@qq.com>
2014-02-28 14:45:40 +0800

    本地提交master

--- src/org/branch1.java
+++ src/org/branch1.java
@@ -2,8 +2,6 @@ package org;
 
 public class branch1 {
 	public static void main(String[] args) {
-		System.out.println("------branch1------");
-		System.out.println("------branch2------");
-		System.out.println("------branch3------");
+		System.out.println("------master------");
 	}
 }
